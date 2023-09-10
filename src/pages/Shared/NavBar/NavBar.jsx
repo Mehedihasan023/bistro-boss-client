1bfdc4f867dcff62ac4dc230a1b88031602aa6b3
Mehedihasan023 <mh20973@gmail.com>
2023-09-11 01:50:31 +0600

    private route and password verification added

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,11 +1,29 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error))
+    }
     const navOptions = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/menu'>Our Menu</Link></li>
         <li><Link to='/order/salad'>Order Food</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/secret'>secret</Link></li>
+        {
+            user ? <>
+                <span>{user?.displayName}</span>
+                <li onClick={handleLogOut}><Link >Log Out</Link></li>
+            </> :
+                <>
+                    <li><Link to='/login'>Login</Link></li>
+                </>
+        }
     </>
     return (
         <>
@@ -16,14 +34,14 @@ const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
-                           {navOptions}
+                            {navOptions}
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 ">
-                      {navOptions}
+                        {navOptions}
                     </ul>
                 </div>
                 <div className="navbar-end">
