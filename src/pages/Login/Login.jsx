b357707c618b0449fe3efc5913401fb82f0dddb5
Mehedihasan023 <mh20973@gmail.com>
2023-09-13 02:39:08 +0600

    dashboard and mycart delete items action added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -81,8 +81,9 @@ const Login = () => {
                                 </label>
                                 <input onBlur={handleValidateCaptcha} type="text" name="captcha" placeholder="type the captcha above" className="input input-bordered" />
                             </div>
+                            {/* To Do: use disabled state on captcha validation  */}
                             <div className="form-control mt-6">
-                                <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
+                                <input disabled={false} className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
                         <p><small>New Here? <Link to='/signup'>Create an account</Link></small></p>
